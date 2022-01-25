ea18479c7a041f849fdb5e34d32db89a85c115ae
nikita539 <evteev_n@inbox.ru>
2022-01-25 16:01:08 +0300

    crate components folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css'
-
-const App = () => {
-    return (
-        <div className='hello'>
-            Hello World!
-        </div>
-    )
-}
+import App from "./components/App";
 
 ReactDOM.render(<App/>, document.getElementById('root'))
